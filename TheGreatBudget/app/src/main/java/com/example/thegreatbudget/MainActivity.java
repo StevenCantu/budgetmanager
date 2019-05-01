6d5cbc363223e80f016afdfc31cb6144984c2dde
Cantu <Cantu_Steven@live.com>
2019-04-30 18:45:23 -0700

    Testing

--- TheGreatBudget/app/src/main/java/com/example/thegreatbudget/MainActivity.java
+++ TheGreatBudget/app/src/main/java/com/example/thegreatbudget/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
@@ -16,4 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
         Log.i(TAG, "onCreate: it works");
     }
+
+    // TODO: 4/27/2019 github branching.
+    // TODO: 4/27/2019 android studio fragments
+    // TODO: 4/27/2019 tablayout android studio
 }
