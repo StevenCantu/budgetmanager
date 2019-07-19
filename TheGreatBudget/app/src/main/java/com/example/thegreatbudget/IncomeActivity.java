0736125ea4f6b01dcc0f9cb79337686d29df4167
Cantu <Cantu_Steven@live.com>
2019-07-18 17:37:11 -0700

    Keypad logic, layout organized.

--- TheGreatBudget/app/src/main/java/com/example/thegreatbudget/IncomeActivity.java
+++ TheGreatBudget/app/src/main/java/com/example/thegreatbudget/IncomeActivity.java
@@ -8,6 +8,9 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -20,10 +23,15 @@ import static java.lang.Math.pow;
 
 public class IncomeActivity extends AppCompatActivity {
     private static final String TAG = "IncomeActivity";
+    public static final int KEYPAD_INDEX = 11;
+    public static final int INCOME_LIMIT = 10;
 
-    private TextView mIncomeText;
-    private Button mOne, mTwo, mThree, mFour, mFive, mSix, mSeven, mEight, mNine, mZero, mDecimal,
-    mDelete;
+    private ImageView mDivider;
+    private GridLayout mGrid;
+    private TextView mIncomeText, mIncomeInput;
+    private Button mEnter;
+    private Button[] inputs = new Button[11];
+    private ImageButton mDelete;
     private float mIncome;
     private List<Integer> numArray = new ArrayList<>();
 
@@ -35,33 +43,25 @@ public class IncomeActivity extends AppCompatActivity {
         Intent intent = getIntent();
         mIncome = intent.getFloatExtra(MainActivity.INCOME_EXTRA, 0f);
 
-        mOne = findViewById(R.id.button1);
-        mTwo = findViewById(R.id.button2);
-        mThree = findViewById(R.id.button3);
-        mFour = findViewById(R.id.button4);
-        mFive = findViewById(R.id.button5);
-        mSix = findViewById(R.id.button6);
-        mSeven = findViewById(R.id.button7);
-        mEight = findViewById(R.id.button8);
-        mNine = findViewById(R.id.button9);
-        mDecimal = findViewById(R.id.buttonDecimal);
-        mZero = findViewById(R.id.button0);
+        for (int i = 0; i < KEYPAD_INDEX; i++) {
+            String buttonID = "button" + i;
+            if (i == 10) {
+                buttonID = "buttonDecimal";
+            }
+            int resourceID = getResources().getIdentifier(buttonID, "id", getPackageName());
+            inputs[i] = findViewById(resourceID);
+            inputs[i].setOnClickListener(keypadListener);
+
+        }
+        mIncomeInput = findViewById(R.id.income_input);
+        mDivider = findViewById(R.id.income_divider);
+        mGrid = findViewById(R.id.keypadLayout);
         mDelete = findViewById(R.id.buttonDelete);
+        mEnter = findViewById(R.id.buttonEnter);
         mIncomeText = findViewById(R.id.income_text);
         updateIncome(mIncome);
         mIncomeText.setOnClickListener(incomeTextClickListener);
-        mOne.setOnClickListener(oneListener);
-        mTwo.setOnClickListener(oneListener);
-        mThree.setOnClickListener(oneListener);
-        mFour.setOnClickListener(oneListener);
-        mFive.setOnClickListener(oneListener);
-        mSix.setOnClickListener(oneListener);
-        mSeven.setOnClickListener(oneListener);
-        mEight.setOnClickListener(oneListener);
-        mNine.setOnClickListener(oneListener);
-        mDecimal.setOnClickListener(oneListener);
-        mZero.setOnClickListener(oneListener);
-        mDelete.setOnClickListener(oneListener);
+        mDelete.setOnClickListener(keypadListener);
 
     }
 
@@ -73,7 +73,7 @@ public class IncomeActivity extends AppCompatActivity {
         }
     };
 
-    View.OnClickListener oneListener = new View.OnClickListener() {
+    View.OnClickListener keypadListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
 //            mIncome += Float.parseFloat(mOne.getText().toString());
@@ -85,109 +85,70 @@ public class IncomeActivity extends AppCompatActivity {
         }
     };
 
-    private void buttonPicker(View v){
-        switch (v.getId()){
-            case R.id.button1:
-                int num = Integer.parseInt(mOne.getText().toString());
-                numArray.add(num);
-                Log.i(TAG, "buttonPicker: 1" + numArray);
-                break;
-            case R.id.button2:
-                num = Integer.parseInt(mTwo.getText().toString());
-                numArray.add(num);
-                Log.i(TAG, "buttonPicker: 2"+ numArray);
-                break;
-            case R.id.button3:
-                num = Integer.parseInt(mThree.getText().toString());
-                numArray.add(num);
-                Log.i(TAG, "buttonPicker: 3"+ numArray);
-                break;
-            case R.id.button4:
-                num = Integer.parseInt(mFour.getText().toString());
-                numArray.add(num);
-                Log.i(TAG, "buttonPicker: 4"+ numArray);
-                break;
-            case R.id.button5:
-                num = Integer.parseInt(mFive.getText().toString());
-                numArray.add(num);
-                Log.i(TAG, "buttonPicker: 5"+ numArray);
-                break;
-            case R.id.button6:
-                num = Integer.parseInt(mSix.getText().toString());
-                numArray.add(num);
-                Log.i(TAG, "buttonPicker: 6"+ numArray);
-                break;
-            case R.id.button7:
-                num = Integer.parseInt(mSeven.getText().toString());
-                numArray.add(num);
-                Log.i(TAG, "buttonPicker: 7"+ numArray);
-                break;
-            case R.id.button8:
-                num = Integer.parseInt(mEight.getText().toString());
-                numArray.add(num);
-                Log.i(TAG, "buttonPicker: 8"+ numArray);
-                break;
-            case R.id.button9:
-                num = Integer.parseInt(mNine.getText().toString());
-                numArray.add(num);
-                Log.i(TAG, "buttonPicker: 9"+ numArray);
-                break;
+    private void buttonPicker(View v) {
+        int num;
+        switch (v.getId()) {
             case R.id.buttonDecimal:
-                double test = parseArray(numArray);
+                double test = 555555;
                 Log.i(TAG, "buttonPicker: test " + test);
                 break;
             case R.id.button0:
-                num = Integer.parseInt(mZero.getText().toString());
+                num = Integer.parseInt(((Button) v).getText().toString());
                 if (!numArray.isEmpty()) {
-                    numArray.add(num);
+                    addtoNumArray(num);
                 }
-                Log.i(TAG, "buttonPicker: 0"+ numArray);
+                Log.i(TAG, "buttonPicker: " + arrayToInt(numArray));
                 break;
             case R.id.buttonDelete:
-                if(!numArray.isEmpty()) {
+                if (!numArray.isEmpty()) {
                     numArray.remove(numArray.size() - 1);
+                    Log.i(TAG, "buttonPicker: " + numArray.toString());
                 }
                 break;
             default:
+                num = Integer.parseInt(((Button) v).getText().toString());
+                addtoNumArray(num);
+                Log.i(TAG, "buttonPicker: " + arrayToInt(numArray));
                 break;
         }
     }
 
-    private void setButtonsVisibility(final int visibility){
-        mOne.setVisibility(visibility);
-        mTwo.setVisibility(visibility);
-        mThree.setVisibility(visibility);
-        mFour.setVisibility(visibility);
-        mFive.setVisibility(visibility);
-        mSix.setVisibility(visibility);
-        mSeven.setVisibility(visibility);
-        mEight.setVisibility(visibility);
-        mNine.setVisibility(visibility);
-        mDecimal.setVisibility(visibility);
-        mZero.setVisibility(visibility);
+    private void addtoNumArray(int num) {
+        if (numArray.size() < INCOME_LIMIT) {
+            numArray.add(num);
+        }
+    }
+
+    private void setButtonsVisibility(final int visibility) {
+        for (int i = 0; i < KEYPAD_INDEX; i++) {
+            inputs[i].setVisibility(visibility);
+        }
+        mIncomeInput.setVisibility(visibility);
+        mDivider.setVisibility(visibility);
+        mEnter.setVisibility(visibility);
+        mGrid.setVisibility(visibility);
         mDelete.setVisibility(visibility);
 
     }
 
-    private void updateIncome(float value){
+    private void updateIncome(float value) {
         NumberFormat numberFormat = NumberFormat.getCurrencyInstance(Locale.US);
         String money = numberFormat.format(value);
         mIncomeText.setText(String.format(Locale.US, "Income: %s", money));
     }
 
-    private double parseArray(List<Integer> nums){
-        double result = 0;
-        for(int i = 0; i < nums.size(); i++){
-            int power = nums.size() - (i +1);
-            result += nums.get(i) * pow(10, power);
-        }
-        return result;
-    }
-
-
-    private void moveIncomeView(final int gravity){
-        RelativeLayout.LayoutParams p = (RelativeLayout.LayoutParams)mIncomeText.getLayoutParams();
+    private void moveIncomeView(final int gravity) {
+        RelativeLayout.LayoutParams p = (RelativeLayout.LayoutParams) mIncomeText.getLayoutParams();
         mIncomeText.setGravity(gravity);
         mIncomeText.setLayoutParams(p);
+    }
+
+    private long arrayToInt(List<Integer> list){
+        long total = 0;
+        for (Integer i : list) { // assuming list is of type List<Integer>
+            total = 10*total + i;
+        }
+        mIncomeInput.setText(String.valueOf(total));
+        return total;
     }
 }
